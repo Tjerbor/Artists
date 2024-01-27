f9a7d69953bdfeec5abfec8e92c313df2aad5ef2
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-01-27 19:10:12 +0100

    2024-01-27 19:10

--- #random artist.bat
+++ #random artist.bat
@@ -1,6 +1,2 @@
-set regular="artist to check out regularly.txt"
-set next="artist to check out next.txt"
-set randomizer="Artist_List_Randomizer.jar"
-
-java -jar %randomizer% %regular% %next%
+artists.exe
 @pause
