e7bf3696647aed81c26fa7759809064ae207e408
Thiemo Ottersbach <thiemo.ott@gmail.com>
2025-03-11 21:38:29 +0100

    cleanup: remove scdl_archive from pyinstall

--- #pyinstall.bat
+++ #pyinstall.bat
@@ -2,9 +2,9 @@ set script=artists
 pyinstaller --onefile %script%.py
 copy .\dist\%script%.exe .\%script%.exe
 
-set script=scdl_archive
-pyinstaller --onefile %script%.py
-copy .\dist\%script%.exe .\%script%.exe
+Rem set script=scdl_archive
+Rem pyinstaller --onefile %script%.py
+Rem copy .\dist\%script%.exe .\%script%.exe
 
 rmdir /Q /S build dist
 del /f *.spec
