10f1883dc79f906ebdbce7a1c33433556ab672b5
Thiemo Ottersbach <thiemo.ott@gmail.com>
2025-09-09 23:45:40 +0200

    tkinter sucks, last time im using it

--- src/artist.py
+++ src/artist.py
@@ -1,19 +1,25 @@
+from ast import alias
 from datetime import date
-from encodings import aliases
+import datetime
+import json
 import tkinter as tk
 
 
 class Alias:
+
     def __init__(
         self,
-        aliases: list,  # parent list self is part of
         alias: str,
+        parent_artist=None,  # parent artist self is part of
         default_copy_to_clipboard: bool = False,
     ):
-        self.aliases = aliases
+        self.parent_artist = parent_artist
         self.alias = tk.StringVar(value=alias)
         self.default_copy_to_clipboard = tk.BooleanVar(value=default_copy_to_clipboard)
         self.add_bool_tracer()
+
+    def set_parent_artist(self, value):
+        self.parent_artist = value
 
     def get_alias(self):
         return self.alias
@@ -31,28 +37,45 @@ class Alias:
         return self.default_copy_to_clipboard
 
     def add_bool_tracer(self):
-        def on_boolean_update(var, index, mode):
+        def on_boolean_update_alias(var, index, mode):
             self_val = self.default_copy_to_clipboard.get()
-
-            for aliax in self.aliases:
-                aliax.set_default_copy_to_clipboard(False)
             if self_val:
-                self.default_copy_to_clipboard.set(True)
+                self.parent_artist.set_new_default(self)
+            else:
+                if not self.parent_artist.has_default_copy_to_clipboard():
+                    self.parent_artist.make_first_alias_default()
 
-        self.default_copy_to_clipboard.trace_add("write", callback=on_boolean_update)
+        self.default_copy_to_clipboard.trace_add(
+            "write", callback=on_boolean_update_alias
+        )
 
     def __repr__(self):
         return f"<Alias alias:{self.alias.get()} bool:{self.default_copy_to_clipboard.get()}>"
+
+    def to_json(self):
+        json = {"alias": self.alias.get()}
+        if self.is_default_copy_to_clipboard():
+            json["is_default"] = self.default_copy_to_clipboard.get()
+        return json
 
 
 class Link:
 
     def __init__(
-        self, link: str, note: str = None, default_copy_to_clipboard: bool = False
+        self,
+        link: str,
+        parent_artist=None,  # parent artist self is part of
+        note: str = None,
+        default_copy_to_clipboard: bool = False,
     ):
+        self.parent_artist = parent_artist
         self.link = tk.StringVar(value=link)
         self.note = tk.StringVar(value=note)
         self.default_copy_to_clipboard = tk.BooleanVar(value=default_copy_to_clipboard)
+        self.add_bool_tracer()
+
+    def set_parent_artist(self, value):
+        self.parent_artist = value
 
     def get_note(self):
         return self.note
@@ -61,7 +84,7 @@ class Link:
         self.note.set(value)
 
     def has_note(self):
-        return self.note.get() is not None or self.note.get != ""
+        return self.note.get() is not None and self.note.get() != ""
 
     def get_link(self):
         return self.link
@@ -75,8 +98,32 @@ class Link:
     def set_default_copy_to_clipboard(self, value):
         self.default_copy_to_clipboard.set(value)
 
+    def get_default_copy_to_clipboard(self):
+        return self.default_copy_to_clipboard
+
+    def add_bool_tracer(self):
+        def on_boolean_update_link(var, index, mode):
+            self_val = self.default_copy_to_clipboard.get()
+            if self_val:
+                self.parent_artist.set_new_default(self)
+            else:
+                if not self.parent_artist.has_default_copy_to_clipboard():
+                    self.parent_artist.make_first_alias_default()
+
+        self.default_copy_to_clipboard.trace_add(
+            "write", callback=on_boolean_update_link
+        )
+
     def __repr__(self):
         return f"<Link link:{self.link.get()} bool:{self.default_copy_to_clipboard.get()} note:{self.note.get()}>"
+
+    def to_json(self):
+        json = {"link": self.link.get()}
+        if self.is_default_copy_to_clipboard():
+            json["is_default"] = self.default_copy_to_clipboard.get()
+        if self.has_note():
+            json["note"] = self.note.get()
+        return json
 
 
 class Artist:
@@ -88,22 +135,26 @@ class Artist:
         date_last_checked: date = None,
     ):
 
-        aliaxes = []
-        for i, alias in enumerate(aliases):
-            if i == 0:
-                aliaxes.append(Alias(aliaxes, alias, True))
+        self.aliases = []
+        for alias in aliases:
+            if isinstance(alias, str):
+                self.aliases.append(Alias(alias, self))
             else:
-                aliaxes.append(Alias(aliaxes, alias, False))
-
-        self.aliases = aliaxes
+                alias.set_parent_artist(self)
+                self.aliases.append(alias)
 
         self.note = tk.StringVar(value=note)
 
-        if links is not None:
-            self.links = list(map(lambda a: Link(a), links))
+        self.links = []
+        for link in links:
+            if isinstance(link, str):
+                self.links.append(Link(link, self))
+            else:
+                link.set_parent_artist(self)
+                self.links.append(link)
 
-        else:
-            self.links = links
+        if not self.has_default_copy_to_clipboard():
+            self.make_first_alias_default()
 
         self.date_last_checked = date_last_checked
 
@@ -113,6 +164,14 @@ class Artist:
     def set_aliases(self, value):
         self.aliases = value
 
+    def add_alias(self, alias):
+        self.aliases.append(Alias(alias, self, False))
+
+    def remove_alias(self, index):
+        del self.aliases[index]
+        if not self.has_default_copy_to_clipboard():
+            self.make_first_alias_default()
+
     def get_note(self):
         return self.note
 
@@ -120,7 +179,7 @@ class Artist:
         self.note.set(value)
 
     def has_note(self):
-        return self.note.get() is not None or self.note.get() != ""
+        return self.note.get() is not None and self.note.get() != ""
 
     def get_links(self):
         return self.links
@@ -128,8 +187,17 @@ class Artist:
     def set_links(self, value):
         self.links = value
 
+    def add_link(self, link):
+        self.links.append(Link(link, self, default_copy_to_clipboard=False))
+
+    def remove_link(self, index):
+        del self.links[index]
+        if not self.has_default_copy_to_clipboard():
+            self.make_first_alias_default()
+
     def has_links(self):
-        return self.links is not None or len(self.links) > 0
+        if self.links is not None:
+            return len(self.links) > 0
 
     def get_date_last_checked(self):
         return self.date_last_checked
@@ -142,12 +210,113 @@ class Artist:
 
     def has_default_copy_to_clipboard(self) -> bool:
         alias_bools = [
-            alias.is_default_copy_to_clipboard() for alias in self.get_ALIASES()
+            alias.is_default_copy_to_clipboard() for alias in self.get_aliases()
         ]
-        links_bools = [link.is_default_copy_to_clipboard() for link in self.get_LINKS()]
+        if self.has_links():
+            links_bools = [
+                link.is_default_copy_to_clipboard() for link in self.get_links()
+            ]
+            all_bools = alias_bools.extend(links_bools)
 
-        all_bools = alias_bools.extend(links_bools)
+        all_bools = alias_bools
         return True in all_bools
+
+    def set_new_default(self, var_new_default):
+        if self.has_links():
+            aliases_and_links = self.aliases[
+                :
+            ]  # shallow copy because extend() returns none
+            aliases_and_links.extend(self.links)
+
+        else:
+            aliases_and_links = self.aliases
+
+        for alias_link in aliases_and_links:
+            if (
+                alias_link.is_default_copy_to_clipboard()
+                and alias_link is not var_new_default
+            ):
+                alias_link.set_default_copy_to_clipboard(False)
+        # var_new_default.set_default_copy_to_clipboard(True)
+
+        # for aliax in self.parent_artist.get_aliases():
+        #     if aliax.is_default_copy_to_clipboard():
+        #         aliax.set_default_copy_to_clipboard(False)
+        #     self.default_copy_to_clipboard.set(True)
+
+    def make_first_alias_default(self):
+        if len(self.aliases) > 0:
+            self.aliases[0].set_default_copy_to_clipboard(True)
 
     def __repr__(self):
         return f"<Aritst Aliases:{self.aliases} Note:{self.note.get()} Links:{self.links} last_checked:{self.date_last_checked}>"
+
+    def to_json(self):
+        json = {"aliases": [alias.to_json() for alias in self.aliases]}
+        if self.has_note():
+            json["note"] = self.note.get()
+        if self.has_links():
+            json["links"] = [link.to_json() for link in self.links]
+        if self.has_date_last_checked():
+            json["date_last_checked"] = self.date_last_checked
+        return json
+
+
+def write_data_file(list_of_artists):
+    json_artists = json.dumps(
+        {"artists": [artist.to_json() for artist in list_of_artists]}
+    )
+    with open(
+        f"data/backup_{datetime.datetime.now().timestamp()}.json", "w", encoding="utf-8"
+    ) as backup_file:
+        backup_file.write(json_artists)
+    with open("data/data.json", "w", encoding="utf-8") as data_file:
+        data_file.write(json_artists)
+    return json_artists
+
+
+def load_data_file(filepath):
+    artists = []
+    with open(filepath, "r", encoding="utf-8") as data_file:
+        artists_json = json.load(data_file)
+    for artist in artists_json["artists"]:
+        aliases = []
+        for alias in artist["aliases"]:
+            alias_string = alias["alias"]
+            if "is_default" in alias.keys():
+                alias_object = Alias(
+                    alias=alias_string, default_copy_to_clipboard=alias["is_default"]
+                )
+            else:
+                alias_object = Alias(alias=alias_string)
+            aliases.append(alias_object)
+
+        links = []
+        if "links" in artist.keys():
+            for link in artist["links"]:
+                link_str = link["link"]
+                link_object = Link(link=link_str)
+                if "note" in link.keys():
+                    link_object.set_note(link["note"])
+                if "is_default" in link.keys():
+                    link_object.set_default_copy_to_clipboard(link["is_default"])
+                links.append(link_object)
+
+        note = None
+        if "note" in artist.keys():
+            note = artist["note"]
+
+        date_last_checked = None
+        if "date_last_checked" in artist.keys():
+            date_last_checked = artist["date_last_checked"]
+
+        artists.append(
+            Artist(
+                aliases=aliases,
+                note=note,
+                links=links,
+                date_last_checked=date_last_checked,
+            )
+        )
+
+    return artists
--- src/interface.py
+++ src/interface.py
@@ -1,4 +1,12 @@
-from artist import Artist
+import datetime
+from functools import partial
+import json
+from pydoc import text
+from random import Random, random
+import webbrowser
+
+import pyperclip
+from artist import Artist, load_data_file, write_data_file
 
 import tkinter as tk
 
@@ -15,15 +23,40 @@ def main():
         f"{int(screen_width* 2/3)}x{int(screen_height* 2/3)}+{int(screen_width * 1/6)}+{int(screen_height*1/6)}"
     )
 
-    frame = tk.Frame(root, width=200, height=200)
-    frame.pack(padx=10, pady=10)
+    canvas = tk.Canvas(root, width=200, height=200)
+    canvas.pack(padx=10, pady=10)
 
-    a = Artist(
-        ["hi", "isisiu", "laskdjflsa", "alsdkfj", "alsdjfl;askjdf"],
-        links=["https://iwiwiwi.net"],
-    )
+    def on_closing():
+        # print(write_data_file(runtime_artists))
+        write_data_file(runtime_artists)
+        root.destroy()
 
-    create_artist_frame(a, frame)
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+
+    # runtime_artists = [
+    #     Artist(
+    #         ["hi", "1", "2", "3", "4"],
+    #         links=[
+    #             "https://iwiwiwi.net",
+    #             "https://www.youtube.com/watch?v=jE-SpRI3K5g",
+    #         ],
+    #     ),
+    #     Artist(["damn son"]),
+    # ]
+
+    runtime_artists = load_data_file("data/data.json")
+    # runtime_artists = []
+    # with open(
+    #     "data/artist to check out regularly.txt", "r", encoding="utf-8"
+    # ) as regular:
+    #     runtime_artists.extend(
+    #         list(map(lambda x: Artist([x]), regular.read().splitlines()))
+    #     )
+
+    for artist in runtime_artists:
+        orderframe = tk.Frame(canvas, bg="grey", padx=10, pady=5)
+        orderframe.pack()
+        create_artist_frame(artist, orderframe)
 
     # image = tk.PhotoImage(file="bleh.png")
     # tk.Label(frame, image=image).pack()
@@ -32,9 +65,14 @@ def main():
 
 
 def create_artist_frame(artist, root):
-    artist_frame = tk.Frame(root)
-    artist_frame.pack()
-    alias_frame = tk.Frame(artist_frame)
+    artist_top_frame = tk.Frame(root, bg="skyblue")
+    artist_top_frame.pack()
+    create_alias_frame(artist, artist_top_frame)
+    create_links_frame(artist, root)
+
+
+def create_alias_frame(artist, root):
+    alias_frame = tk.Frame(root)
     alias_frame.pack()
     alias_label = tk.Label(alias_frame, text="Alias")
     alias_label.pack(anchor="s")
@@ -43,13 +81,19 @@ def create_artist_frame(artist, root):
     additional_aliases_frame = tk.Frame(alias_frame, padx=10, pady=5)
     additional_aliases_frame.pack()
 
-    widgets = {}
-
     add_button_index = len(artist.get_aliases())
+
+    def copy_alias(alias_variable):
+        pyperclip.copy(alias_variable.get())
+
+    def open_url(url):
+        webbrowser.open(url)
+
     for i, alias in enumerate(artist.get_aliases()):
 
         if i == 0:
             frame = tk.Frame(first_alias_frame, padx=10)
+            frame.pack()
             # first alias
 
             aliasvar = alias.get_alias()
@@ -57,13 +101,22 @@ def create_artist_frame(artist, root):
                 frame,
                 textvariable=aliasvar,
             )
-            entry.pack(side=tk.LEFT)
+            entry.pack(side=tk.LEFT, fill="x", expand=True)
 
             def show_update(*args):
                 print(aliasvar.get())
                 print(artist)
 
             aliasvar.trace("w", show_update)
+
+            copy_button = tk.Button(
+                frame,
+                bg="skyblue",
+                text="C",
+                compound=tk.LEFT,
+                command=partial(copy_alias, aliasvar),
+            )
+            copy_button.pack(side=tk.LEFT, fill="x", expand=True)
 
             # necessary step, otherwise artist won't get updated
             boolvar = alias.get_default_copy_to_clipboard()
@@ -72,8 +125,27 @@ def create_artist_frame(artist, root):
                 frame,
                 variable=boolvar,
             )
-            checkbox.pack()
-            frame.pack()
+            checkbox.pack(side=tk.LEFT, fill="x", expand=True)
+
+            soundcloud_button = tk.Button(
+                frame,
+                bg="#FF6103",
+                text="sc",
+                command=partial(
+                    open_url, f"https://soundcloud.com/search/people?q={aliasvar.get()}"
+                ),
+            )
+            soundcloud_button.pack(side=tk.LEFT, fill="x", expand=True)
+
+            deezer_button = tk.Button(
+                frame,
+                bg="#8A2BE2",
+                text="D",
+                command=partial(
+                    open_url, f"https://www.deezer.com/search/{aliasvar.get()}"
+                ),
+            )
+            deezer_button.pack(side=tk.LEFT, fill="x", expand=True)
 
         # second alias
         else:
@@ -84,7 +156,7 @@ def create_artist_frame(artist, root):
                 frame,
                 textvariable=aliasvar,
             )
-            entry.pack(side=tk.LEFT)
+            entry.pack(side=tk.LEFT, fill="x", expand=True)
 
             def show_update(*args):
                 print(aliasvar.get())
@@ -106,18 +178,108 @@ def create_artist_frame(artist, root):
 
             # widgets[str(boolvar)] = boolvar
 
+            copy_button = tk.Button(
+                frame,
+                bg="skyblue",
+                text="C",
+                compound=tk.LEFT,
+                command=partial(copy_alias, aliasvar),
+            )
+            copy_button.pack(side=tk.LEFT, fill="x", expand=True)
+
+            def remove_alias(index: int):
+                artist.remove_alias(index)
+                alias_frame.destroy()
+                create_alias_frame(artist, root)
+
             checkbox = tk.Checkbutton(frame, variable=boolvar)
             checkbox.pack(side=tk.LEFT)
-            button = tk.Button(frame, bg="red")
+            button = tk.Button(
+                frame, bg="red", text="-", command=partial(remove_alias, i)
+            )
             button.pack(side=tk.LEFT)
-
             frame.grid(column=i, row=0)
 
     add_button_frame = tk.Frame(additional_aliases_frame, padx=10)
     add_button_frame.grid(column=add_button_index, row=0)
+
+    def add_alias():
+        artist.add_alias("")
+        alias_frame.destroy()
+        create_alias_frame(artist, root)
+
     add_button = tk.Button(
         add_button_frame,
-        bg="green",
+        command=add_alias,
+        text="+",
+        bg="light green",
+    )
+
+    add_button.pack()
+
+
+def create_links_frame(artist, root):
+    links_top_frame = tk.Frame(root, bg="skyblue")
+    links_top_frame.pack()
+    label = tk.Label(links_top_frame, text="Links")
+    label.pack()
+    links_frame = tk.Frame(links_top_frame)
+    links_frame.pack()
+
+    def copy_link(link_variable):
+        pyperclip.copy(link_variable.get())
+
+    def open_link(link_varible):
+        webbrowser.open(link_varible.get())
+
+    add_button_index = len(artist.get_links())
+    for i, link in enumerate(artist.get_links()):
+        frame = tk.Frame(links_frame)
+        frame.pack()
+
+        notevar = link.get_note()
+        note_entry = tk.Entry(frame, textvariable=notevar)
+        note_entry.grid(column=0, row=i)
+
+        linkvar = link.get_link()
+        link_entry = tk.Entry(frame, textvariable=linkvar)
+        link_entry.grid(column=1, row=i)
+
+        copy_button = tk.Button(
+            frame, bg="skyblue", text="C", command=partial(copy_link, linkvar)
+        )
+        copy_button.grid(column=2, row=i)
+
+        open_link_button = tk.Button(
+            frame, bg="grey", text="->", command=partial(open_link, linkvar)
+        )
+        open_link_button.grid(column=3, row=i)
+
+        boolvar = link.get_default_copy_to_clipboard()
+
+        checkbox = tk.Checkbutton(frame, variable=boolvar)
+        checkbox.grid(column=4, row=i)
+
+        def remove_link(index):
+            artist.remove_link(index)
+            links_top_frame.destroy()
+            create_links_frame(artist, root)
+
+        delete_button = tk.Button(
+            frame, bg="red", text="-", command=partial(remove_link, i)
+        )
+        delete_button.grid(column=5, row=i)
+
+    add_button_frame = tk.Frame(links_frame)
+    add_button_frame.pack()
+
+    def add_link():
+        artist.add_link("")
+        links_top_frame.destroy()
+        create_links_frame(artist, root)
+
+    add_button = tk.Button(
+        add_button_frame, bg="light green", text="C", command=add_link
     )
     add_button.pack()
 
